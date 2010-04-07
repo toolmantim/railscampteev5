c5889fa62344ae898607304a2621685448e6878a
Tim Lucas <t.lucas@toolmantim.com>
2010-04-07 13:58:53 +1000

    Get the big shiny button going on the reservation form too

--- public/v5.js
+++ public/v5.js
@@ -1,6 +1,15 @@
 (function(){
   $(function() {
-    return $("a.order-now").click(function() {
+    $("a.order-now").click(function() {
+      var form;
+      form = $("#order-form");
+      form.show();
+      setTimeout((function() {
+        return form.addClass("visible").find("input:first").focus();
+      }), 50);
+      return false;
+    });
+    return $("#order-form").submit(function() {
       return false;
     });
   });
--- views/v5.coffee
+++ views/v5.coffee
@@ -1,3 +1,10 @@
 $ ->
   $("a.order-now").click ->
+    form: $("#order-form")
+    form.show()
+    setTimeout (-> form.addClass("visible").find("input:first").focus()), 50
+    
+    return false
+
+  $("#order-form").submit ->
     return false
