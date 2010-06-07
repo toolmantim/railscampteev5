fc6728bced7b64a81c21005d620a3a725ae33677
Tim Lucas <t.lucas@toolmantim.com>
2010-06-07 18:12:05 +1000

    Don't post to Wufoo

--- railscampteev5.rb
+++ railscampteev5.rb
@@ -7,20 +7,20 @@ require 'sinatra'
 require 'net/http'
 require 'uri'
 
-raise "No WUFOO_API_KEY found in ENV" unless ENV["WUFOO_API_KEY"]
+# raise "No WUFOO_API_KEY found in ENV" unless ENV["WUFOO_API_KEY"]
 
 get '/' do
   haml :v5
 end
 
 post '/order' do
-  Net::HTTP.post_form URI.parse("http://toolmantim.wufoo.com/api/insert/"),
-                      {
-                        "w_api_key" => ENV["WUFOO_API_KEY"],
-                        "w_form" => "railscamp-tee-v5",
-                        "4" => params[:name],
-                        "3" => params[:sex],
-                        "5" => params[:size]
-                      }
+  # Net::HTTP.post_form URI.parse("http://toolmantim.wufoo.com/api/insert/"),
+  #                     {
+  #                       "w_api_key" => ENV["WUFOO_API_KEY"],
+  #                       "w_form" => "railscamp-tee-v5",
+  #                       "4" => params[:name],
+  #                       "3" => params[:sex],
+  #                       "5" => params[:size]
+  #                     }
   "All good!"
 end
